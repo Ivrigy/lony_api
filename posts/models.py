c572bcf2f23ebf1213328e10e3eaf119ca5b7dfa
“Ivona” <ivona.rieger@gmail.com>
2025-05-07 14:14:50 +0200

    added image_filter and serializers

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,22 @@ class Post(models.Model):
     Default image set so that we can always reference image.url.
     """
 
+    image_filter_choices = [
+        ("_1977", "1977"),
+        ("brannan", "Brannan"),
+        ("earlybird", "Earlybird"),
+        ("hudson", "Hudson"),
+        ("inkwell", "Inkwell"),
+        ("lofi", "Lo-Fi"),
+        ("kelvin", "Kelvin"),
+        ("normal", "Normal"),
+        ("nashville", "Nashville"),
+        ("rise", "Rise"),
+        ("toaster", "Toaster"),
+        ("valencia", "Valencia"),
+        ("walden", "Walden"),
+        ("xpro2", "X-pro II"),
+    ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -17,6 +33,9 @@ class Post(models.Model):
         upload_to="images/",
         default="https://res.cloudinary.com/dhhna0y51/image/upload/v1746523937/default_post_vpjyvz.jpg",
         blank=True,
+    )
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default="normal"
     )
 
     class Meta:
