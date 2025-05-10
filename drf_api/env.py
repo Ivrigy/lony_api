ecd4fb604d03024d1c170f30542fb519406ae39d
“Ivona” <ivona.rieger@gmail.com>
2025-05-10 11:14:43 +0200

    fixed bugs and depreciation

--- drf_api/env.py
+++ drf_api/env.py
@@ -1,4 +1,8 @@
 import os
 
-os.environ['CLOUDINARY_URL'] = 'cloudinary://<your_api_key>:<your_api_secret>@dhhna0y51'
-os.environ['DEV'] = '1'
+os.environ["CLOUDINARY_URL"] = "cloudinary://<your_api_key>:<your_api_secret>@dhhna0y51"
+os.environ['SECRET_KEY'] = 'django-insecure-6wb#ip0z2!i%o*pqxqwu4wmwrn)l_y!dnzo_k=#f*@%ttv2e!o'
+# os.environ['DEV'] = '1'
+os.environ["DATABASE_URL"] = (
+    "postgresql://neondb_owner:npg_KlRpLg4eP1Ys@ep-purple-wind-a2qd67lw.eu-central-1.aws.neon.tech/rinse_stamp_water_942885"
+)
