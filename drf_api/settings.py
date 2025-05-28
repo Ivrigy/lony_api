73b3e8183cb77f0846a9f8d2661188f33d5bd677
“Ivona” <ivona.rieger@gmail.com>
2025-05-29 00:25:17 +0200

    add static

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -181,6 +181,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
