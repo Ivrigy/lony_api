f52449ef30807387492fd8e269e1d867a13b5e5b
“Ivona” <ivona.rieger@gmail.com>
2025-05-28 18:49:19 +0200

    update and format urls

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -20,16 +20,14 @@ from django.urls import path, include
 from .views import root_route
 
 urlpatterns = [
-    path('', root_route),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
-    path('', include('profiles.urls')),
-    path('', include('posts.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('followers.urls')),
+    path("", root_route, name="root"),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/auth/", include("dj_rest_auth.urls")),
+    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("", include("profiles.urls")),
+    path("", include("posts.urls")),
+    path("", include("comments.urls")),
+    path("", include("likes.urls")),
+    path("", include("followers.urls")),
 ]
